737fcd212f437c1ce918db9f324eac2299136397
Taner ÇAVDAR <tanercavdar@gmail.com>
2019-06-18 19:02:12 +0300

    Create DateTimeUtils.java

---
+++ DateTimeUtils.java
@@ -0,0 +1,40 @@
+package tr.com.bracket.trade.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTimeUtils {
+
+    public Date stringToDate(String sDate, String sInputFormat) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat(sInputFormat);
+            return format.parse(sDate);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String getNowDate(String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date());
+    }
+
+    public static String getNowTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    public static String stringToString(String date, String inputFormat, String outputFormat) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat(inputFormat);
+            Date newDate = null;
+            newDate = format.parse(date);
+            format = new SimpleDateFormat(outputFormat);
+            return format.format(newDate);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+}
